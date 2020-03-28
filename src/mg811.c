d58d3ce335a532d4078d1595ce1b958464c0efb6
Iván Vergara <ivan.and.26@gmail.com>
2020-03-28 12:54:01 -0500

    Ajuste de la curva del sensor MG811

--- src/mg811.c
+++ src/mg811.c
@@ -3,7 +3,7 @@
  * @file mg811.c
  * @author Ingeniería Apropiada
  * @date 26/03/2020
- * @brief File containing ADC driver.
+ * @brief File containing MG811 driver.
  */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@
 /* ************************************************************************** */
 /* ************************************************************************** */
 
-uint32_t value_ppm_CO2;
+uint32_t value_ppm_CO2; /**< Calculated CO2 ppm value. */
+float CO2Curve[3] = {2.602, ZERO_POINT_VOLTAGE, (REACTION_VOLTGAE/(2.602-3))};  /**< Data format:{ x, y, slope}, a line is formed which is "approximately equivalent" to the original curve. */
 
 /* ************************************************************************** */
 /* ************************************************************************** */
@@ -43,9 +44,18 @@ uint32_t value_ppm_CO2;
 /* ************************************************************************** */
 
 void get_sample_ppm_CO2(void) { /**< Calculate the CO2 ppm value. */
-    /**< Input: 550 - 3320 mV */
-    /**< Output: 350 - 10000 ppm */
-    value_ppm_CO2 = (uint32_t)((34838*adcData.values_2_prom[1] - 15660650)/10000);
+    /**< Input:   volts - MG811 output measured in volts*/
+    /**< Output:  ppm of the target gas */
+    /**< Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
+         of the line could be derived if y(MG-811 output) is provided. As it is a
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
+         value.*/
+        
+    if ( ((adcData.values_2_prom[1]/1000)/DC_GAIN) >= ZERO_POINT_VOLTAGE ) {
+        value_ppm_CO2 = 0;
+    } else {
+        value_ppm_CO2 = (uint32_t)(pow(10, (((adcData.values_2_prom[1]/1000)/DC_GAIN)-CO2Curve[1])/CO2Curve[2] + CO2Curve[0]));
+    }
 }
 
 /* *****************************************************************************
--- src/mg811.h
+++ src/mg811.h
@@ -33,6 +33,9 @@ extern "C" {
     /* ************************************************************************** */
     /* ************************************************************************** */
 
+    #define DC_GAIN             8.5     /**< Define the DC gain of amplifier. */
+    #define ZERO_POINT_VOLTAGE  0.306   /**< Define the output of the sensor in volts when the concentration of CO2 is 400PPM. */
+    #define REACTION_VOLTGAE    0.030   /**< Define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2. */
     
     // *****************************************************************************
     // *****************************************************************************
@@ -40,7 +43,8 @@ extern "C" {
     // *****************************************************************************
     // *****************************************************************************
     
-    extern uint32_t value_ppm_CO2;
+    extern uint32_t value_ppm_CO2;  /**< Calculated CO2 ppm value. */
+    extern float CO2Curve[3];       /**< Data format:{ x, y, slope}, a line is formed which is "approximately equivalent" to the original curve. */
     
     // *****************************************************************************
     // *****************************************************************************
